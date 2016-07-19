e38f36a740754e13fbc7c85b117c13bb300f0afa
Aaron Powell <me@aaron-powell.com>
2016-07-19 21:05:24 +1000

    sample form with basic life cycle

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,60 @@
 import React, {Component} from 'react';
 import { render } from 'react-dom';
 
+class FormItemComponent extends Component {
+    render () {
+        return (
+            <fieldset>
+                <label htmlFor={this.props.id}>{this.props.label}: </label>
+                <input type={this.props.type} id={this.props.id} value={this.props.value} onChange={(e) => this.props.change(e.target.value)} />
+            </fieldset>
+        );
+    }
+}
+
+FormItemComponent.defaultProps = {
+    type: 'text',
+    vale: ''
+};
 
 class LifeCycledComponent extends Component {
-    render() {
-        return (
-            <div>
+    constructor (props) {
+        super(props);
 
-            </div>
+        this.state = {
+            username: '',
+            password: ''
+        };
+    }
+
+
+    componentWillMount () {
+        fetch('http://httpstat.us/200')
+            .then((res) => this.setState({ status: res.status }));
+    }
+
+
+    render () {
+        var formProps = {};
+
+        if (this.state.status !== 200) {
+            formProps.disabled = true;
+        }
+
+        return (
+            <form {...formProps}>
+                <FormItemComponent label="Username" id="username" value={this.state.username} change={(value) => this.change('username', value)} />
+                <FormItemComponent label="Password" id="password" type="password" value={this.state.password} change={(value) => this.change('password', value)} />
+
+                <fieldset>
+                    <button type="submit">Login</button>
+                </fieldset>
+            </form>
         );
+    }
+
+    change (target, value) {
+        this.setState({ [target]: value });
     }
 }
 
